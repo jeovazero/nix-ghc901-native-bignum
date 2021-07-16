6dee17f6e69065536cac8a6e5d142fb89d0a95cc
jeovazero <contato@jeova.ninja>
2021-07-15 22:49:39 -0300

    feat: add pinned nixpkgs

---
+++ pinned.nix
@@ -0,0 +1,6 @@
+builtins.fetchTarball {
+    name = "nixos-21.05-2021-07-01";
+    url = "https://github.com/nixos/nixpkgs/archive/21b696caf392ad6fa513caf3327d0aa0430ffb72.tar.gz";
+    # Hash obtained using `nix-prefetch-url --unpack <url>`",
+    sha256 = "1056r3383aaf5zhf7rbvka76gqxb8b7rwqxnmar29vxhs9h56m5k";
+}
