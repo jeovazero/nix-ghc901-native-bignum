aeb8d5e696eb98f752989d3f616d4a7cd1c6ba0e
jeovazero <contato@jeova.ninja>
2021-07-17 23:23:28 -0300

    feat: add the default.nix

---
+++ default.nix
@@ -0,0 +1,38 @@
+let
+    inherit (import <nixpkgs> {}) pkgs;
+    inherit (pkgs) lib;
+
+    flavors = {
+        gmp = import ./ghc-flavors/gmp.nix;
+        nativeBignum = import ./ghc-flavors/native-bignum.nix;
+        nativeBignumWithPatch = import ./ghc-flavors/native-bignum-with-patch.nix;
+    };
+
+    verboseOpt = "--ghc-option=-v";
+
+    statify =
+        { pkgs, haskellPackages }:
+        { app, withGmp ? false, verbose ? false, extraFlags ? [] }:
+        let 
+            app' = haskellPackages.callCabal2nix app.name app.src {};
+            gmpLibOpt = "--ghc-option=-optl=-L${pkgs.gmp6.override { withStatic = true; }}/lib";
+        in
+            pkgs.haskell.lib.overrideCabal app' (drv: {
+              enableSharedExecutables = false;
+              enableSharedLibraries = false;
+              configureFlags = [
+                "--ghc-option=-optl=-static"
+                "--ghc-option=-optl=-L${pkgs.glibc.static}/lib"
+                "--ghc-option=-optl=-L${pkgs.zlib.static}/lib"
+                "--ghc-option=-optl=-L${pkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
+              ] ++ pkgs.lib.optional verbose verboseOpt 
+                ++ pkgs.lib.optional (builtins.trace withGmp withGmp) gmpLibOpt
+                ++ extraFlags;
+            });
+in {
+    inherit statify flavors;
+    genStatic =
+        builtins.mapAttrs
+            (name: value: statify (lib.getAttrs ["pkgs" "haskellPackages"] value))
+            flavors;
+}
--- static.nix
+++ static.nix
@@ -2,13 +2,13 @@ let
   nixpkgs = import <nixpkgs> {};
   inherit (nixpkgs) pkgs;
 
-  gmp = import ./ghc-flavors/gmp.nix;
-  native = import ./ghc-flavors/native-bignum.nix;
-  nativeWithPatch = import ./ghc-flavors/native-bignum-with-patch.nix;
+  utils = import ./default.nix;
+  inherit (utils) genStatic;
 
-  genStatic = import ./gen-static.nix;
-in {
-  gmp = genStatic { mypkgs = gmp; withGmp = true; };
-  native = genStatic { mypkgs = native; };
-  nativeWithPatch = genStatic { mypkgs = nativeWithPatch; };
-}
+  app = { name = "app"; src = ./app; };
+
+  params = { app = app; verbose = true; };
+in
+    builtins.mapAttrs
+        (name: statify: statify (params // { withGmp = name == "gmp"; }))
+        genStatic
